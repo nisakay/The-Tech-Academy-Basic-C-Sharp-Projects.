c015e021678b00b19d4514d7909c8a12666f7ba1
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-10-21 14:23:59 -0700

    Initial Commit

---
+++ WelcomeScreen/WelcomeScreen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WelcomeScreen
+{
+    class Program
+    {
+        static void Main()
+        {
+            string greetings = "Welcome to Acme Accounting Systems \nRemember, we’re “accounting” on you!";
+            Console.WriteLine(greetings);
+            Console.ReadLine();
+        }
+    }
+}
