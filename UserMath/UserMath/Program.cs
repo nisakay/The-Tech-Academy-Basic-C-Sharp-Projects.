b102daf2f4c225ad769e6b505874c88269453efc
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-11-11 02:35:27 -0800

    Initial commit

---
+++ UserMath/UserMath/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace UserMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Enter a number:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int num1 = 50;
+            double result = x * num1;
+            Console.WriteLine("Your number times 50 =" + result);
+            Console.ReadLine();
+
+            //Console.WriteLine("Enter a number:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int num1 = 25;
+            //int result = x * num1;
+            //Console.WriteLine("Your number plus 25 =" + result);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter a number:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //double num2 = 12.5;
+            //double result = x / num2;
+            //Console.WriteLine("Your number divided by 12.5 =" + result);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter a number:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int num1 = 50;
+            //bool result = x > num1;
+            //Console.WriteLine("Your number greater than 50 =" + result);
+            //Console.ReadLine();
+
+            //Console.WriteLine("Enter a number:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+            //int num1 = 7;
+            //int result = x % num1;
+            //Console.WriteLine("The remainder of Your is number =" + result);
+            //Console.ReadLine();
+        }
+    }
+}
