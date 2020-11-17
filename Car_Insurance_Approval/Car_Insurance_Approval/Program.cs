b053ea217242a2c73a503c2e461977c6f323deda
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-11-17 02:35:14 -0800

    second commit

---
+++ Car_Insurance_Approval/Car_Insurance_Approval/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace booleanLogic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Whatis your age?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Please answer “true” or “false.");
+            bool DUI = Convert.ToBoolean(Console.ReadLine());
+
+            Console.WriteLine("How many speeding tickets do you have? ");
+            int tickets = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 15 || DUI != false || tickets > 3)
+            {
+                Console.WriteLine("You do not Qualify for Car insurance");
+            }
+            else
+            {
+                Console.WriteLine("You Qualify for Car insurance");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
