f9187a04d63677e659876519fa2a25f56200244a
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2020-11-17 02:06:07 -0800

    second commit

--- Loops/Loops/Program.cs
+++ Loops/Loops/Program.cs
@@ -6,21 +6,61 @@ namespace Loops
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("Guess a number");
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //bool isGuessed = number == 12;
+
+            //do
+            //{
+            //    switch (number)
+            //    {
+            //        case 62:
+            //            Console.WriteLine("You guessed 62. Try again");
+            //            Console.WriteLine("Guess a number");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 29:
+            //            Console.WriteLine("You guessed 29. Try again");
+            //            Console.WriteLine("Guess a number");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 55:
+            //            Console.WriteLine("You guessed 55. Try again");
+            //            Console.WriteLine("Guess a number");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //        case 12:
+            //            Console.WriteLine("You guessed 12. That is correct!");
+            //            isGuessed = true;
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are wrong.");
+            //            Console.WriteLine("Guess a number");
+            //            number = Convert.ToInt32(Console.ReadLine());
+            //            break;
+            //    }
+            //}
+
+            //    while (!isGuessed) ;
+
+            //Console.ReadLine();
+
             Console.WriteLine("Guess a number");
             int number = Convert.ToInt32(Console.ReadLine());
-            bool isGuessed = number == 12;
+            bool isGuessed = number == 15;
 
-            do
+            while (!isGuessed)
             {
+
                 switch (number)
                 {
-                    case 62:
-                        Console.WriteLine("You guessed 62. Try again");
+                    case 52:
+                        Console.WriteLine("You guessed 52. Try again");
                         Console.WriteLine("Guess a number");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
-                    case 29:
-                        Console.WriteLine("You guessed 29. Try again");
+                    case 30:
+                        Console.WriteLine("You guessed 30. Try again");
                         Console.WriteLine("Guess a number");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
@@ -29,8 +69,8 @@ namespace Loops
                         Console.WriteLine("Guess a number");
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
-                    case 12:
-                        Console.WriteLine("You guessed 12. That is correct!");
+                    case 15:
+                        Console.WriteLine("You guessed 15. That is correct!");
                         isGuessed = true;
                         break;
                     default:
@@ -39,11 +79,10 @@ namespace Loops
                         number = Convert.ToInt32(Console.ReadLine());
                         break;
                 }
+                
             }
-
-                while (!isGuessed) ;
-
             Console.ReadLine();
+
         }
-    }
+        }
 }
