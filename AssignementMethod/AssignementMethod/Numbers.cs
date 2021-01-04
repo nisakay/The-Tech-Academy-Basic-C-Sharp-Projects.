f9f48315d195d7ae7e394f660e0af80594a5fcdd
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-04 14:18:36 -0800

    Initial Commit

---
+++ AssignementMethod/AssignementMethod/Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignementMethod
+{
+    public class Numbers
+    {
+        public int Addition(int x)
+        {
+            return x + 5;
+        }
+
+        public int Multipliy(int x)
+        {
+            return x * 5;
+        }
+
+        public int Division(int x)
+        {
+            return x / 5;
+        }
+
+    }
+}
---
+++ AssignementMethod/AssignementMethod/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AssignementMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            Numbers myMath = new Numbers();
+
+                int result = myMath.Addition(userInput);
+                int result2 = myMath.Multipliy(userInput);
+                int result3 = myMath.Division(userInput);
+
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+        }    
+    }
+}
