35992e06b1a0d4fc40c9e2ce8da0d03a55f8ffa2
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-05 23:27:05 -0800

    Initial Commit

---
+++ assignmentMethod2/assignmentMethod2/Calculus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignmentMethod2
+{
+    public class Calculus
+    {
+        public int Addition(int x)
+        {
+            return x + 5;
+        }
+
+        public double Addition(double y)
+        {
+            return y - 5;
+        }
+
+        public int Addition(string z)
+        {
+            int A = Convert.ToInt32(z);
+            return A + 5;
+        }
+    }
+}
---
+++ assignmentMethod2/assignmentMethod2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace assignmentMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculus a = new Calculus();
+            int x = a.Addition(12);
+            double y = a.Addition(12.5);
+            int A = a.Addition("2");
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(A);
+
+        }
+    }
+}
