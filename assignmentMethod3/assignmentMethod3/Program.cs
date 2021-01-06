f7a10e2cda971de2b212e912a74754a9d5b612f5
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-06 01:14:17 -0800

    Initial Commit

---
+++ assignmentMethod3/assignmentMethod3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace assignmentMethod3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = Convert.ToInt32(Console.ReadLine());
+
+
+
+
+
+            TwoIntegers myMath = new TwoIntegers();
+
+            int result = myMath.Addition(x,y);
+
+            Console.WriteLine(result);
+
+
+
+        }
+    }
+}
---
+++ assignmentMethod3/assignmentMethod3/TwoIntegers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignmentMethod3
+{
+    public class TwoIntegers
+    {
+        public int Addition (int x, int y = 0)
+        {
+            return x + y;
+        }
+    }
+}
