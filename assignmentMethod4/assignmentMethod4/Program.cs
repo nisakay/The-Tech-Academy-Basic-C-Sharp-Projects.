27f8e0e8de951ef177d5d19f80ebb53a8ae8a5a0
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-06 14:37:23 -0800

    Initial Commit

---
+++ assignmentMethod4/assignmentMethod4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace assignmentMethod4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            TwoIntegers a = new TwoIntegers();
+
+
+            int sum = a.Addition(12);
+            int num2 = a.Addition(10);
+
+            Console.WriteLine(sum);
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ assignmentMethod4/assignmentMethod4/TwoIntegers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignmentMethod4
+{
+    public class TwoIntegers
+    {
+        public static void Addition(int num1, int num2)
+        {
+            int sum = 0;
+            sum = num1 + 5;
+
+        }
+    }
+}
