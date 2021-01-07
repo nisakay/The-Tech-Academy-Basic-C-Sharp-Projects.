b68302b93a33f20249ea8ede6b46e1080ecb1d17
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-06 19:17:35 -0800

    Initial Commit

---
+++ assignmentMethod5/assignmentMethod5/Calculus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignmentMethod5
+{
+    public class Calculus
+    {
+        public void Addition(int x)
+        {
+            x = x / 2;
+            Console.WriteLine(x);
+        }
+
+        public int Substraction(int c, int y)
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ assignmentMethod5/assignmentMethod5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace assignmentMethod5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a Number");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Calculus a = new Calculus();
+
+             a.Addition(x);
+        }
+    }
+}
