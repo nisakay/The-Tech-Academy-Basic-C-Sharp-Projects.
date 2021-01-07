abeeee00fbb0bcb9aa9f55636c90936a89fbf9ef
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-07 10:51:09 -0800

    Initial Commit

---
+++ InheritanceAssignment/InheritanceAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InheritanceAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee person = new Employee();
+            person.FirstName = "Sample";
+            person.LastName = "Student";
+            person.SayName();
+            Console.ReadLine();
+        }
+    }
+}
