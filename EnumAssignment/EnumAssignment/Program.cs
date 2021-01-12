bc8e55d8117f51ab370951c40bfd72c4efd5c9e1
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-12 13:12:54 -0800

    First Commit

---
+++ EnumAssignment/EnumAssignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EnumAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Week day = new Week();
+
+            foreach(string day in day)
+                try
+                {
+                        Console.WriteLine("enter the current day of the week.", day);
+                        Console.ReadLine();
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Please enter an actual day of the week." , day);
+                }
+       
+        }
+    }
+}
---
+++ EnumAssignment/EnumAssignment/Week.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnumAssignment
+{
+    public class Week
+    {
+        public enum DaysOfTheWeek
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+}
