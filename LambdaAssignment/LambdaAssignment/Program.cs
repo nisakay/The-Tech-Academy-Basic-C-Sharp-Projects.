62745a55dcd6b913480dc7370c586fc5890f03c7
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-13 01:01:39 -0800

    First Commit

---
+++ LambdaAssignment/LambdaAssignment/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LambdaAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> empList = new List<Employee>
+
+                {
+                    new Employee { FirstName = "Joe", LastName = "Swanson", ID = 1},
+                    new Employee { FirstName = "John", LastName = "Robertson", ID = 2 },
+                    new Employee { FirstName = "Joe", LastName = "Holladay", ID = 3},
+                    new Employee { FirstName = "Mike", LastName = "Johnson", ID = 4},
+                    new Employee { FirstName = "Chris", LastName = "Lamarcus", ID = 5},
+                    new Employee { FirstName = "Tom", LastName = "Dwight", ID = 6},
+                    new Employee { FirstName = "Liam", LastName = "Brown", ID = 7},
+                    new Employee { FirstName = "Caleb", LastName = "Kim", ID = 8},
+                    new Employee { FirstName = "Steve", LastName = "Danscott", ID = 9},
+                    new Employee { FirstName = "Nolan", LastName = "McDonald", ID = 10}
+                };
+
+                foreach (Employee Employee in FirstName)
+                {
+                    Func<Employee, bool> Employee = s => s.FirstName == "Joe";
+                    {
+                        Console.WriteLine(Employee);
+                    }
+                }
+                
+
+
+        }
+    }
+}
