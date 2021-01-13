2fabc5600262ef316018d0d9524bef93bedcfe26
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-12 16:59:14 -0800

    Initial Commit

---
+++ StructAssignment/StructAssignment/Calculus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StructAssignment
+{
+    public struct Calculus
+    {
+          public double Amount { get; set; }
+    }
+}
---
+++ StructAssignment/StructAssignment/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculus a = new Calculus();
+
+            a.Amount = 3.5;
+
+            Console.WriteLine(a.Amount);
+        }
+    }
+}
