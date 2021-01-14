7125efaa19ddce0cd4e12b2e7293a88a4a7cf74c
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-14 15:43:57 -0800

    First Commit

---
+++ VariableAssignment/VariableAssignment/Construct.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VariableAssignment
+{
+    public class Construct
+    {
+        public class ChainConstruct
+        {
+            public ChainConstruct() : this(10)
+            {
+                  
+            }
+
+            public ChainConstruct(int Age)
+            {
+                  
+            }
+        }
+    }
+}
---
+++ VariableAssignment/VariableAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariableAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string parkName = "Jurasik Park";
+
+            var i = 10;
+
+            Console.WriteLine("Welcome to {0} let's start this adventure" , parkName);
+
+            Console.WriteLine(i);
+        }
+    }
+}
