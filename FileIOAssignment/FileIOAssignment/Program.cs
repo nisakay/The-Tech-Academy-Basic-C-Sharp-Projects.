8c40d75478ad71c11230e688d0b0fe0b322a99be
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-13 17:41:45 -0800

    First Commit

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(number);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\lione\Documents\IO.txt", true))
+            {
+                file.WriteLine(number);
+            }
+
+            
+
+
+        }
+    }
+
+}
