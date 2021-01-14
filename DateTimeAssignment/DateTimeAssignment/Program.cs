d5710cf2d8a4755de888ccbfbe3c4d112cdc6ea0
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-13 18:45:34 -0800

    First Commit

---
+++ DateTimeAssignment/DateTimeAssignment/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DateTimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Enter a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            DateTime newTime = DateTime.Now.AddHours(number);
+
+            Console.WriteLine(newTime);
+        }
+    }
+}
