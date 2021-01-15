d1ba9e8e42ca26494ebebb3489b1199aa747e473
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-14 18:03:53 -0800

    First Commit

---
+++ TryCatchAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                 Console.WriteLine("How old are you");
+                int age = Convert.ToInt32(Console.ReadLine());
+
+                int currentYear = 2021;
+
+                int YourAge = currentYear - age;
+
+                if (age <= 0)
+                {
+                    throw new ArgumentException();
+                }
+                Console.WriteLine("You were born in {0}", YourAge);
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please do not enter zero or negative numbers.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occured");
+                Console.ReadLine();
+                return;
+            }
+        }
+    }
+}
