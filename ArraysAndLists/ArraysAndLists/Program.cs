e6ab3b67ac0a2b9610f5f0ae4e6c3d2ed502f31f
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-23 00:06:17 -0800

    Add files via upload

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -9,19 +9,23 @@ class Program
 
         //Create a one-dimensional Array of strings
         string[] nameArray = new string[] { "Isaac", "Chris", "Marc" };
-            string index = Console.ReadLine();
-            Console.WriteLine("Select a name "+nameArray[index]);
+        Console.WriteLine("Select a name ");
+        int index = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(nameArray[index]);
 
-        //Create a one-dimensional Array of integer
-        int[] numArray = new int[] {2,8,60,87,23,103};
-        int index = Console.ReadLine();
-        Console.WriteLine("Select a number " + numArray[index]);
+        //Create a one - dimensional Array of integer
+        int[] numArray2 = new int[] { 2, 8, 60, 87, 23, 103 };
+        Console.WriteLine("Select a number ");
+        int index2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(numArray2[index2]);
 
         List<string> intList = new List<string>();
         intList.Add("I'm a software developer");
         intList.Add("lebron James");
-        intList.Remove("Michael Jordan);
-        Console.WriteLine("Select a sentence "+intList[0]);
+        intList.Remove("Michael Jordan");
+        Console.WriteLine("Select a sentence ");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(intList[x]);
         Console.ReadLine();
 
 
