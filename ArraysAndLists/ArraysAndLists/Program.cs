7eef7da4321c6a8464b64786ac2155c115649f8a
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-24 04:04:52 -0800

    if else statements

--- ArraysAndLists/ArraysAndLists/Program.cs
+++ ArraysAndLists/ArraysAndLists/Program.cs
@@ -11,13 +11,28 @@ class Program
         string[] nameArray = new string[] { "Isaac", "Chris", "Marc" };
         Console.WriteLine("Select a name ");
         int index = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(nameArray[index]);
+        if (index >= 0 && index <= 2)
+        {
+            Console.WriteLine(nameArray[index]);
+        }
+        else
+        {
+            Console.WriteLine("Sorry, but the number you entered is out of bounds of the array");
+        }
 
         //Create a one - dimensional Array of integer
         int[] numArray2 = new int[] { 2, 8, 60, 87, 23, 103 };
         Console.WriteLine("Select a number ");
         int index2 = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(numArray2[index2]);
+        if (index2 >= 0 && index2 <= 6)
+        {
+            Console.WriteLine(numArray2[index2]);
+        }
+        else
+        {
+            Console.WriteLine("Sorry, but the number you entered is out of bounds of the array");
+        }
+
 
         List<string> intList = new List<string>();
         intList.Add("I'm a software developer");
@@ -25,7 +40,14 @@ class Program
         intList.Remove("Michael Jordan");
         Console.WriteLine("Select a sentence ");
         int x = Convert.ToInt32(Console.ReadLine());
-        Console.WriteLine(intList[x]);
+        if (x >= 0 && x <= 1)
+        {
+            Console.WriteLine(intList[x]);
+        }
+        else
+        {
+            Console.WriteLine("Sorry, but the number you entered is out of bounds of the List");
+        }
         Console.ReadLine();
 
 
