8b5fbff326295aa4a94c9501aa029587e3a4f71e
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-24 04:35:29 -0800

    added if else statement

--- assignmentMethod3/assignmentMethod3/Program.cs
+++ assignmentMethod3/assignmentMethod3/Program.cs
@@ -8,17 +8,24 @@ namespace assignmentMethod3
         {
             Console.WriteLine("Enter a Number");
             int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a second Number");
             int y = Convert.ToInt32(Console.ReadLine());
 
-
-
-
-
             TwoIntegers myMath = new TwoIntegers();
+            if (y == 0) 
+            {
+                int result = myMath.Addition(x);
 
-            int result = myMath.Addition(x);
+                Console.WriteLine(result);
 
-            Console.WriteLine(result);
+            }
+            else  
+            {
+                int result = myMath.Addition(x,y);
+
+                Console.WriteLine(result);
+
+            }
 
 
 
