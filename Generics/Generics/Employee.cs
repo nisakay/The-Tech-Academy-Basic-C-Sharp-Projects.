937bfb0fb09fe52ecf1d0e2f679d862e2178cd38
Nisa Kayoulou <54015132+nisakay@users.noreply.github.com>
2021-01-29 02:28:01 -0800

    Initial Commit

---
+++ Generics/Generics/Employee.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Drill22Generics
+{
+    //NOTE: Here I am unsing generics in this class and its list property.
+    class Employee<T>
+    {
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System.Collections.Generic; //NOTE: This is a must when using Generics (eg. Lists)
+using System;
+
+namespace Drill22Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee<string> employee = new Employee<string>();
+
+            //NOTE: Since 'Things' is just a property at this point, it must be instantiated before being able to add elements to it.
+            employee.Things = new List<string>();
+
+            employee.Things.Add("Thing1");
+            employee.Things.Add("Thing2");
+            employee.Things.Add("Thing3");
+
+            Employee<int> employee2 = new Employee<int>();
+            employee2.Things = new List<int>();
+            employee2.Things.Add(1);
+            employee2.Things.Add(2);
+            employee2.Things.Add(3);
+
+            Console.WriteLine("These are all the things from the list of strings, 'employee.Things':");
+            foreach (string thing in employee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.WriteLine("\nThese are all the things from the list of ints, 'employee.Things':");
+            foreach (int thing in employee2.Things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
